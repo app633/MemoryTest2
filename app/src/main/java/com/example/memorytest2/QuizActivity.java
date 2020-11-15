770bd0ac80aed4e1bfae30a3c6c520c91906653b
noaun <noaunkown@gmail.com>
2020-11-15 17:08:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/memorytest2/QuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.memorytest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
